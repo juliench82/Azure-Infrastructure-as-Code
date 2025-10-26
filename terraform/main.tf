aef059902b898421cf9ea2c3133239f94753ea1e
Julien Chevallier <106062222+juliench82@users.noreply.github.com>
2025-10-26 20:38:53 +0100

    Add parameters example for ARM and Terraform starter for balance

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "iac-demo-rg"
+  location = "West Europe"
+}
